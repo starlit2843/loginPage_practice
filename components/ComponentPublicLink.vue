4455460ede0d25be6fd3246f8ae0d2f1cd60aca2
Amy Lu <starlit2843@gmail.com>
2024-10-17 09:15:56 +0800

    update components

---
+++ components/ComponentPublicLink.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-col gap-1">
+      <h1 class="font-bold text-base text-base-100">公開連結</h1>
+      <a :href="link" rel="noopener noreferrer" target="_blank" class="flex flex-col gap-1">
+        <span class="truncate rounded-full bg-base px-4 py-2 text-base-content shadow-special-cta-02">
+          {{ link }}
+        </span>
+      </a>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    link: {
+      type: String,
+      required: true,
+    },
+  });
+  </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
-  compatibilityDate: '2024-10-11'
+  compatibilityDate: '2024-10-11',
+  components: true
 })
