b4da09c7aefe18070c82717acf6e3d7f8a20fa4d
Amy Lu <starlit2843@gmail.com>
2024-10-24 07:29:12 +0800

    create swiper and home.vue

---
+++ components/swiper.vue
@@ -0,0 +1,70 @@
+<template>
+    <swiper
+      @slideChange="onSlideChange"
+      class="swiper"
+      :loop="true"
+      :navigation="true"
+      :modules="[Autoplay, Navigation, Pagination, EffectFade]"
+      :pagination="{ clickable: true, hideOnClick: true }"
+      :slides-per-view="1"
+      :autoplay="{ delay: 3000, disableOnInteraction: false }"
+      effect="fade"
+    >
+      <swiper-slide>
+        <img src="~/assets/images/starSign1.png" class="h-50 w-50" alt="starSign1 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign2.png" class="h-50 w-50" alt="starSign2 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign3.png" class="h-50 w-50" alt="starSign3 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign4.png" class="h-50 w-50" alt="starSign4 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign5.png" class="h-50 w-50" alt="starSign5 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign6.png" class="h-50 w-50" alt="starSign6 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign7.png" class="h-50 w-50" alt="starSign7 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign8.png" class="h-50 w-50" alt="starSign8 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign9.png" class="h-50 w-50" alt="starSign9 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign10.png" class="h-50 w-50" alt="starSign10 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign11.png" class="h-50 w-50" alt="starSign11 pic">
+      </swiper-slide>
+      <swiper-slide>
+        <img src="~/assets/images/starSign12.png" class="h-50 w-50" alt="starSign12 pic">
+      </swiper-slide>
+    </swiper>
+  </template>
+  
+  <script setup>
+  import '~/assets/css/tailwind base.css';
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import { Autoplay, Navigation, Pagination, EffectFade } from 'swiper/modules';
+  
+  import 'swiper/css';
+  import 'swiper/css/navigation';
+  import 'swiper/css/pagination';
+  import 'swiper/css/autoplay';
+  import 'swiper/css/effect-fade';
+  
+  const onSlideChange = () => {
+    console.log('slide changed');
+  };
+  </script>
+
+  <style>
+    
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+     <div class="h-[100vh] mx-auto flex max-w-lg grow flex-col overflow-auto">
+        <div class="flex h-full flex-col gap-[10vh] overflow-y-scroll bg-[url('~/assets/images/UserPageBackground.png')] px-9 py-[5vh]">
+            <div class="items-center justify-center">
+                <ComponentSwiper></componentSwiper>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import ComponentSwiper from '~/components/swiper.vue';
+</script>
