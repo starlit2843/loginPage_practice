ba77ee83ab92bbd03eef27928ea89232ef4d71eb
Amy Lu <starlit2843@gmail.com>
2024-10-27 18:11:35 +0800

    add vue.tour

--- pages/home.vue
+++ pages/home.vue
@@ -1,4 +1,5 @@
 <template>
+    
      <div class="h-[100vh] mx-auto flex max-w-lg grow flex-col overflow-auto">
         <div class="flex h-full flex-col gap-[10vh] overflow-y-scroll bg-[url('~/assets/images/UserPageBackground.png')] px-9 py-[5vh]">
             <div class="items-center justify-center">
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <template>
     <div id="app" class="mx-auto flex grow flex-col overflow-auto bg-fullscreen flex h-full gap-[10vh] bg-[url('~/assets/images/HomepageBG.png')] px-9 py-[5vh] ">
-      <div class="flex justify-center items-center ">
-        <div class="grid grid-cols-2 gap-10 place-items-center">
-          <div id="login_Button" class=" shadow-special-cta-02 rounded-30 relative top-[80vh] login_Button" >
+      <div id="aa" class="flex justify-center items-center ">
+
+
+        <div>
+          <VOnboardingWrapper ref="wrapper" :steps="steps" />
+
+          <div class="grid grid-cols-2 gap-10 place-items-center">
+            <div id="login_Button" class=" shadow-special-cta-02 rounded-30 relative top-[80vh] login_Button" >
               <button @click="redirectToLogin" class=" rounded-30 px-6 py-2 hover:underline:hover">
                 <span class="text-[4vw] text-white min-[512px]:text-xl">LOG IN
                 </span>
@@ -13,8 +18,11 @@
                 <span class="text-[4vw] text-white min-[512px]:text-xl">Home
                 </span>
             </button>
+            </div>
           </div>
+
         </div>
+
       </div>
     </div>
 
@@ -22,6 +30,13 @@
 
 <script setup>
 //import { ref } from 'vue';
+
+
+import { ref, onMounted } from 'vue';
+import { VOnboardingWrapper, useVOnboarding } from 'v-onboarding';
+import 'v-onboarding/dist/style.css';
+
+
 import '~/assets/css/tailwind base.css';
 import{useRouter} from 'vue-router';
 
@@ -33,6 +48,52 @@ function redirectToLogin(){
 function redirectToHomePage(){
   router.push('/home');
 }
+
+
+const wrapper = ref(null);
+const { start, goToStep, finish } = useVOnboarding(wrapper);
+
+const steps = ref([
+  { 
+    attachTo: { element: '#aa' }, 
+    content: { title: 'Welcome to our Page!!' } 
+  },
+  { 
+    attachTo: { element: '#login_Button' }, 
+    content: { 
+      title: 'Hit here to Log in!', 
+    } 
+  },
+  { 
+    attachTo: { element: '#home_Button' }, 
+    content: { title: 'Go to Home Page!' } 
+  }
+]);
+
+const options = ref({
+  popper: {
+    modifiers: [
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 5]
+        }
+      }
+    ]
+  },
+  labels: {
+    previousButton: '上一步',
+    nextButton: '下一步',
+    finishButton: '完成'
+  }
+});
+
+onMounted(() => {
+  start();
+});
+
+
+
 </script>
 
 <style>
